5e9351245d4d4eb98d1c9e4e6af73d7d0fbe26d2
Jelte Meganck <jelmega@gmail.com>
2024-02-23 20:42:04 +0000

    Removed onca_simd (will be rewritten later)

--- crates/onca_simd/src/backend/x86_64/avx/mod.rs
+++
@@ -1,6 +0,0 @@
-mod convert;
-mod extend_compress;
-mod cmp;
-mod mask_ops;
-mod arith;
-mod misc;
--- crates/onca_simd/src/backend/x86_64/sse/mod.rs
+++
@@ -1,7 +0,0 @@
-mod cmp;
-mod mask_ops;
-mod convert;
-mod extend_compress;
-mod arith;
-
-mod misc;
